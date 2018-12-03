3d459bc60ed27ce1479c3e6fc11243f3f428d48c
CaptainHook16 <45101692+CaptainHook16@users.noreply.github.com>
2018-12-03 02:11:49 +0200

    tagger2 works

--- helper_funcs2.py
+++ helper_funcs2.py
@@ -1,31 +1,35 @@
-import numpy as np
-#this file contains helper functions to deal with the training set and the test set
-
-#let's define some global list:
+# this file contains helper functions to deal with the training set and the test set
+from itertools import izip
+# let's define some global list:
 Representation_Of_Indexes_By_Words = {}
 Representation_Of_Words_By_Indexes = {}
-Representation_Of_classes_By_Indexes ={}
-Representation_Of_Indexes_By_classes ={}
-#define set let us assign an index to each unique word - avoid giving the same word many indexes
+Representation_Of_classes_By_Indexes = {}
+Representation_Of_Indexes_By_classes = {}
+# define set let us assign an index to each unique word - avoid giving the same word many indexes
 Dictionary_of_classes = set()
-Dictionary_of_words = set()
+Dictionary_of_words = []
 
+import numpy as np
 
-#more to define:
-UNK = "UNKNOWN_WORD"
-WINDOW_START = "START_WIN"
+# more to define:
+UNK = "UUUNKKK"
+WINDOW_START = 'START_WIN'
 WINDOW_END = "END_WIN"
 NEW_LINE = "\n"
 TAB = "\t"
 
-def add_class_and_word_to_dics(m_class,word):
+
+def get_word_embeddings_matrix():
+    return np.loadtxt("wordVectors.txt")
+
+def add_class_and_word_to_dics(m_class, word):
     global Dictionary_of_words
     global Dictionary_of_classes
     Dictionary_of_classes.add(m_class)
     Dictionary_of_words.add(word)
 
-def read_train_data(file_name):
 
+def read_train_data(file_name):
     global Dictionary_of_words
     global Dictionary_of_classes
     tagged_sentences = []
@@ -35,15 +39,17 @@ def read_train_data(file_name):
         for line in content:
             if NEW_LINE == line:
                 tagged_sentences.append(sentence_and_tags)
-                sentence_and_tags =[]
+                sentence_and_tags = []
                 continue
             line = clean_line(line)
-            word, tag  = line.split()
-            add_class_and_word_to_dics(tag, word)
-            sentence_and_tags.append((word, tag))
+            word, tag = line.split()
+            #add_class_and_word_to_dics(tag, word)
+            Dictionary_of_classes.add(tag)
+            sentence_and_tags.append((word.lower(), tag))
     Dictionary_of_classes.add(UNK)
-    Dictionary_of_words.add(UNK)
+    #Dictionary_of_words.add(UNK)
     return tagged_sentences
+
 
 def clean_line(line):
     return line.strip(NEW_LINE).strip().strip(TAB)
@@ -63,59 +69,38 @@ def read_dev_data(file_name):
                 continue
             line = clean_line(line)
             word, tag = line.split()
-            sentence_and_tags.append((word, tag))
+            sentence_and_tags.append((word.lower(), tag))
 
     return tagged_sentences
 
 
-
 def updating_dictionaries_set():
-
-
     global Representation_Of_Words_By_Indexes
     global Representation_Of_Indexes_By_Words
     global Representation_Of_classes_By_Indexes
     global Representation_Of_Indexes_By_classes
+    global Dictionary_of_words
     print("here2")
     print(len(Dictionary_of_words))
-    Dictionary_of_words.update(set([WINDOW_START, WINDOW_END]))
+    #Dictionary_of_words.update(set([WINDOW_START, WINDOW_END]))
     print("after2")
     print(len(Dictionary_of_words))
     Representation_Of_Words_By_Indexes = {
-        m_word : m_index for m_index, m_word in enumerate(Dictionary_of_words)
+        m_word: m_index for m_index, m_word in enumerate(Dictionary_of_words)
     }
     Representation_Of_Indexes_By_Words = {
-        m_index : m_word for m_word, m_index in Representation_Of_Words_By_Indexes.iteritems()
+        m_index: m_word for m_word, m_index in Representation_Of_Words_By_Indexes.iteritems()
     }
     Representation_Of_classes_By_Indexes = {
-        m_class : m_index for m_index, m_class in enumerate(Dictionary_of_classes)
+        m_class: m_index for m_index, m_class in enumerate(Dictionary_of_classes)
     }
     Representation_Of_Indexes_By_classes = {
-        m_index : m_class for m_class, m_index in Representation_Of_classes_By_Indexes.iteritems()
+        m_index: m_class for m_class, m_index in Representation_Of_classes_By_Indexes.iteritems()
     }
-
-
-
-def get_word_embeddings_dict_from_file(words_file, vector_file):
-    """
-    get_word_embeddings_dict_from_file function.
-    reads the words and word embeddings vectors and fills word_embeddings_dict
-    :param words_file: name of the file containing the words
-    :param vector_file: name of the file containing the vectors of the words
-    from the words_file
-    :return: word embeddings dictionary
-    """
-    word_embeddings_dict = {}
-    for word, vector_line in izip(open(words_file), open(vector_file)):
-        word = word.strip("\n").strip()
-        vector_line = vector_line.strip("\n").strip().split(" ")
-        word_embeddings_dict[word] = np.asanyarray(map(float,vector_line))
-    return word_embeddings_dict
 
 
 def divide_word_class_sequence_into_windows(word_sequences):
     """
-
        :param word_sequences:
        :return: return an array of windows when each window contain 5 words (as requiredin the assigment)
        """
@@ -123,12 +108,12 @@ def divide_word_class_sequence_into_windows(word_sequences):
     classes = []
     for sentence in word_sequences:
         words_classes_matrix = []
-        #words_classes_matrix = [(WINDOW_START, WINDOW_START), (WINDOW_START, WINDOW_START)]
+        # words_classes_matrix = [(WINDOW_START, WINDOW_START), (WINDOW_START, WINDOW_START)]
         words_classes_matrix.extend([(WINDOW_START, WINDOW_START), (WINDOW_START, WINDOW_START)])
         words_classes_matrix.extend(sentence)
         words_classes_matrix.extend([(WINDOW_END, WINDOW_END), (WINDOW_END, WINDOW_END)])
-        for i, (cure_word,tag) in enumerate(words_classes_matrix):
-            if cure_word!=WINDOW_START and cure_word !=WINDOW_END:
+        for i, (cure_word, tag) in enumerate(words_classes_matrix):
+            if cure_word != WINDOW_START and cure_word != WINDOW_END:
                 new_window = []
                 new_window.append(convert_word_to_index(words_classes_matrix[i - 2][0]))
                 new_window.append(convert_word_to_index(words_classes_matrix[i - 1][0]))
@@ -139,6 +124,7 @@ def divide_word_class_sequence_into_windows(word_sequences):
                 windows_array.append(new_window)
                 classes.append(Representation_Of_classes_By_Indexes[tag])
     return windows_array, classes
+
 
 def divide_word_sequence_into_windows(word_sequences):
     """
@@ -175,21 +161,17 @@ def convert_word_to_index(word_to_convert):
     """
        The first step in using an embedding layer is to encode this sentence by indices.
         In this case we assign an index to each unique word
-
        :param word_to_convert:
        :return: return the index which represent the word.
        """
     if word_to_convert not in Representation_Of_Words_By_Indexes:
-        #if the word is not in the list its index is UNK - Unknown
-        #words we have not seen in the training action we'll map them to the same vector in
-        #the embedding matrix - unk
+        # if the word is not in the list its index is UNK - Unknown
+        # words we have not seen in the training action we'll map them to the same vector in
+        # the embedding matrix - unk
         return Representation_Of_Words_By_Indexes[UNK]
     else:
-        #if the word is in the list, return its index
+        # if the word is in the list, return its index
         return Representation_Of_Words_By_Indexes[word_to_convert]
-
-
-
 
 
 def get_dev_data(file_name):
@@ -206,6 +188,7 @@ def get_dev_data(file_name):
     concat, tags = divide_word_class_sequence_into_windows(tagged_sentences_list)
     return concat, tags
 
+
 def reading_test_dataset(dataset_to_read):
     # define an empty array to return
     word_sequences = []
@@ -217,15 +200,15 @@ def reading_test_dataset(dataset_to_read):
             if NEW_LINE == cure_line:
                 word_sequences.append(sequence)
                 # clean buffer
-                sequence =[]
+                sequence = []
                 # continue yo next iteration of the loop
                 continue
             word_in_sequence = cure_line.strip(NEW_LINE).strip()
-            sequence.append(word_in_sequence)
+            sequence.append(word_in_sequence.lower())
     return word_sequences
 
-def get_train_data(file_name):
 
+def get_train_data(file_name):
     global Dictionary_of_words
     global Dictionary_of_classes
     tagged_sentences_list = read_train_data(file_name)
@@ -234,10 +217,22 @@ def get_train_data(file_name):
     return windows_array, classes
 
 
+
+def get_word_embeddings_dict_from_file(words_file, vector_file):
+
+    word_embeddings_dict = {}
+    for word, vector_line in izip(open(words_file), open(vector_file)):
+        word = word.strip(NEW_LINE).strip()
+        vector_line = vector_line.strip("\n").strip().split(" ")
+        word_embeddings_dict[word] = np.asanyarray(map(float,vector_line))
+        Dictionary_of_words.append(word)
+    return word_embeddings_dict
+
+
+
 def bring_test_data(test_data_set):
     word_sequences = reading_test_dataset(test_data_set)
     return divide_word_sequence_into_windows(word_sequences)
 
-
-
-
+WORD_EMBEDDINGS_DICT = get_word_embeddings_dict_from_file('vocab.txt', 'wordVectors.txt')
+E = get_word_embeddings_matrix()
